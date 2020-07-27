bed73552be0c539ff7d888d2dfb0ee134c45154b
Julián Cortés <pity7736@gmail.com>
2020-07-26 23:03:56 -0500

    feat: added get method to Model

--- kinton/models/model.py
+++ kinton/models/model.py
@@ -1,5 +1,28 @@
+import os
+
+import asyncpg
+from asyncpg import Connection
 from nyoibo import Entity
 
 
 class Model(Entity):
-    pass
+
+    @classmethod
+    async def get(cls, id=None, name=None):
+        if id:
+            field = 'id'
+            param = id
+        else:
+            field = 'name'
+            param = name
+
+        sql = f'select * from categories where {field} = $1'
+        connection: Connection = await asyncpg.connect(
+            host=os.environ['KINTON_HOST'],
+            user=os.environ['KINTON_USER'],
+            database=os.environ['KINTON_DATABASE'],
+            password=os.environ['KINTON_PASSWORD'],
+            port=int(os.environ['KINTON_PORT']),
+        )
+        result = await connection.fetchrow(sql, param)
+        return cls(**result)
--- tests/integration_tests/test_model.py
+++ tests/integration_tests/test_model.py
@@ -1,12 +1,49 @@
+from unittest.mock import AsyncMock
+
+import asyncpg
 from pytest import mark
+
+from kinton import Model, fields
+
+
+class Category(Model):
+    _id = fields.IntegerField()
+    _name = fields.CharField()
+    _description = fields.CharField()
 
 
 @mark.asyncio
-async def test_qwe(db_connection):
-    await db_connection.execute(
-        'insert into categories ("name", "description") values ($1, $2)',
+async def test_get_by_id(db_connection, mocker):
+    connection_mock = mocker.patch.object(asyncpg, 'connect',
+                                          new_callable=AsyncMock)
+    connection_mock.return_value = db_connection
+    result_id = await db_connection.fetchval(
+        'insert into categories ("name", "description") values ($1, $2) '
+        'returning id',
         "test name",
         "test description"
     )
-    r = await db_connection.fetchrow('select * from categories')
-    print(r)
+
+    category = await Category.get(id=result_id)
+
+    assert category.id == result_id
+    assert category.name == 'test name'
+    assert category.description == 'test description'
+
+
+@mark.asyncio
+async def test_get_by_name(db_connection, mocker):
+    connection_mock = mocker.patch.object(asyncpg, 'connect',
+                                          new_callable=AsyncMock)
+    connection_mock.return_value = db_connection
+    result_id = await db_connection.fetchval(
+        'insert into categories ("name", "description") values ($1, $2) '
+        'returning id',
+        "test name",
+        "test description"
+    )
+    category = await Category.get(name='test name')
+
+    assert category.id == result_id
+    assert category.name == 'test name'
+    assert category.description == 'test description'
