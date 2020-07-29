48195d9e3c400a2067d5e52a83d501e7a3b047fd
Julián Cortés <pity7736@gmail.com>
2020-07-28 22:13:32 -0500

    tests: created category fixture
    removed hardcoded sql

--- tests/integration_tests/test_model.py
+++ tests/integration_tests/test_model.py
@@ -14,72 +14,53 @@ class Category(Model):
 
 @fixture
 def db_connection(db_connection, mocker):
-    connection_mock = mocker.patch.object(asyncpg, 'connect',
-                                          new_callable=AsyncMock)
+    connection_mock = mocker.patch.object(asyncpg, 'connect', new_callable=AsyncMock)
     connection_mock.return_value = db_connection
     return db_connection
 
 
-@mark.asyncio
-async def test_get_by_id(db_connection):
-    result_id = await db_connection.fetchval(
-        'insert into category ("name", "description") values ($1, $2) '
-        'returning id',
-        'test name',
-        'test description'
+@fixture
+async def category_fixture(db_connection):
+    return await Category.create(
+        name='test name',
+        description='test description'
     )
 
-    category = await Category.get(id=result_id)
 
-    assert category.id == result_id
+@mark.asyncio
+async def test_get_by_id(category_fixture):
+    category = await Category.get(id=category_fixture.id)
+
+    assert category.id == category_fixture.id
     assert category.name == 'test name'
     assert category.description == 'test description'
 
 
 @mark.asyncio
-async def test_get_by_name(db_connection):
-    result_id = await db_connection.fetchval(
-        'insert into category ("name", "description") values ($1, $2) '
-        'returning id',
-        'test name',
-        'test description'
-    )
+async def test_get_by_name(category_fixture):
     category = await Category.get(name='test name')
 
-    assert category.id == result_id
+    assert category.id == category_fixture.id
     assert category.name == 'test name'
     assert category.description == 'test description'
 
 
 @mark.asyncio
-async def test_get_by_id_and_name(db_connection):
-    result_id = await db_connection.fetchval(
-        'insert into category ("name", "description") values ($1, $2) '
-        'returning id',
-        "test name",
-        "test description"
-    )
+async def test_get_by_id_and_name(category_fixture):
     category = await Category.get(
         name='test name',
         description='test description'
     )
 
-    assert category.id == result_id
+    assert category.id == category_fixture.id
     assert category.name == 'test name'
     assert category.description == 'test description'
 
 
 @mark.asyncio
-async def test_get_without_params(db_connection):
-    result_id = await db_connection.fetchval(
-        'insert into category ("name", "description") values ($1, $2) '
-        'returning id',
-        "test name",
-        "test description"
-    )
-
+async def test_get_without_params(category_fixture):
     category = await Category.get()
-    assert category.id == result_id
+    assert category.id == category_fixture.id
     assert category.name == 'test name'
     assert category.description == 'test description'
 
